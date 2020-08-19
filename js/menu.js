bda477a469e2d9c284a5ddad3a1ec2336036028f
betsegaw-hp <betse53@gmail.com>
2020-08-18 20:15:37 -0700

    second version starts now

--- js/menu.js
+++ js/menu.js
@@ -5,34 +5,26 @@ const menu = [
     category: "breakfast",
     price: 15.99,
     img: "./media/menu-images/item-1.jpeg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed.tundercats freegan. `,
   },
   {
     id: 2,
     title: "diner double",
-    category: "lunch",
+    category: "diner",
     price: 13.99,
     img: "./media/menu-images/item-2.jpeg",
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
   {
     id: 3,
-    title: "godzilla milkshake",
-    category: "shakes",
-    price: 6.99,
-    img: "./media/menu-images/item-3.jpeg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
-  },
-  {
-    id: 4,
     title: "country delight",
     category: "breakfast",
     price: 20.99,
-    img: "../media/menu-images/item-4.jpeg",
+    img: "./media/menu-images/item-4.jpeg",
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
   {
-    id: 5,
+    id: 4,
     title: "egg attack",
     category: "lunch",
     price: 22.99,
@@ -40,23 +32,23 @@ const menu = [
     desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
   },
   {
-    id: 6,
+    id: 5,
     title: "oreo dream",
-    category: "shakes",
+    category: "Budget Meal",
     price: 18.99,
     img: "./media/menu-images/item-6.jpeg",
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
   {
-    id: 7,
+    id: 6,
     title: "bacon overflow",
-    category: "breakfast",
+    category: "diner",
     price: 8.99,
     img: "./media/menu-images/item-7.jpeg",
     desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
   },
   {
-    id: 8,
+    id: 7,
     title: "american classic",
     category: "lunch",
     price: 12.99,
@@ -64,9 +56,9 @@ const menu = [
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
   },
   {
-    id: 9,
+    id: 8,
     title: "quarantine buddy",
-    category: "shakes",
+    category: "Budget Meal",
     price: 16.99,
     img: "./media/menu-images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
@@ -74,15 +66,15 @@ const menu = [
 ];
 
 const articleArea = document.querySelector(".menu-article-area");
+function diplayMenuItems(menuVal) {
+  let displayMenu;
 
-window.addEventListener("DOMContentLoaded", function () {
-  let displayMenu = menu.map(function (item) {
+  displayMenu = menuVal.map( (item) =>{
     // console.log(item);
-
     return `<article class="card-container">
     <div class="card">
       <figure class="figure">
-          <img src=${item.img} class="img" alt=${item.title}>
+          <img src=${item.img} class="card-img" alt=${item.title}>
           <figcaption>
           <h4>${item.title}</h4>
           <h4 class="price">$${item.price}</h4>
@@ -94,7 +86,32 @@ window.addEventListener("DOMContentLoaded", function () {
     </div>
     </article>`;
   });
+
   displayMenu = displayMenu.join("");
   //console.log(displayMenu);
   articleArea.innerHTML = displayMenu;
+}
+window.addEventListener("DOMContentLoaded",  ()=> {
+  diplayMenuItems(menu);
+});
+const menubtn = document.querySelector(".menu-item");
+const menuItems = menubtn.querySelectorAll(".menu-item-list");
+
+menuItems.forEach(Item =>{
+  Item.addEventListener("click",(e)=>{
+    e.preventDefault();
+    console.log(Item);
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter( (menuitem)=> {
+      // console.log(menuItem.category);
+      if (menuitem.category.toLowerCase() === category.toLowerCase()) {
+        return menuitem;
+      }
+    });
+    if (category.toLowerCase() === "all menu") {
+      diplayMenuItems(menu);
+    } else {
+      diplayMenuItems(menuCategory);
+    }
+  });
 });
