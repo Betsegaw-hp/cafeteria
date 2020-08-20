a00b1d976f49d6d43ab84749800448bfcd2d3c9f
betsegaw-hp <betse53@gmail.com>
2020-08-20 03:52:21 -0700

    modified contents

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@
  const bars = document.querySelector(".bars i");
  const listContainer = document.querySelector(".list-container");
  const header = document.querySelector(".header");
-
+ const map = document.querySelector("#google-map"); 
 //  functions
-
+function getWidthInstantly() {
+  console.log(map.clientWidth ,", " + window.innerWidth);
+   map.style.width = (window.innerWidth -100)+"px";
+}
 // event listeners
+window.addEventListener("resize",getWidthInstantly );
+window.addEventListener("change orentation",getWidthInstantly );
 bars.addEventListener("click" ,()=>{
   listContainer.classList.toggle("show-li-container");
   bars.classList.toggle("fa-stream");
