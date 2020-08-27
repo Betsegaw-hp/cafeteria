a3fe8b319b7da847c1ffdbe923303ec33a470de1
betsegaw-hp <betse53@gmail.com>
2020-08-27 12:01:26 -0700

    another update

--- js/app.js
+++ js/app.js
@@ -7,23 +7,65 @@ date.innerHTML = new Date().getFullYear();
  const header = document.querySelector(".header");
  const map = document.querySelector("#google-map"); 
  const preLoad = document.querySelector(".preloader");
+ const bouncer =document.querySelector(".bouncer");
 //  functions
 
 // event listeners
 bars.addEventListener("click" ,()=>{
   listContainer.classList.toggle("show-li-container");
-  bars.classList.toggle("fa-stream");
+  if(bars.classList.contains("fa-stream")) {
+    bars.classList.remove("fa-stream");
+  } else {
+    bars.classList.add("fa-stream");
+  }
 });
+
 window.addEventListener("DOMContentLoaded", ()=>{
   bars.classList.remove("fa-stream");
+  bouncer.classList.add("bouncer-hide");
   preLoad.classList.add("hide-preloader");
 });
+
 window.addEventListener("scroll", ()=> {
   const navHieght = header.scrollHeight;
-
   if(Math.abs(Math.floor(window.scrollY)) >= navHieght) {
     header.classList.add("show-shodow");
-  } else {
+  } 
+  else {
     header.classList.remove("show-shodow");
   }
+  if(Math.abs(Math.floor(window.scrollY)) <= 500 ) {
+    bouncer.classList.add("bouncer-hide");
+  }
+  else {
+    bouncer.classList.remove("bouncer-hide");
+  }
+});
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".a-scroll");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    bars.classList.remove("fa-stream");
+    //navigate to specific spot
+    const id = e.currentTarget.getAttribute("href");
+    const element = document.querySelector(id);
+    const navHeight = header.getBoundingClientRect().height;
+    const containerHeight = listContainer.getBoundingClientRect().height;
+    const fixedNav = header.classList.contains("show-shodow");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    // close
+    listContainer.classList.remove("show-li-container");
+  },false);
 });
